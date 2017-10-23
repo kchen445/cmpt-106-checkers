9f5960e96be3fd242e3b036e8b9a8c42adff89fa
kchen445 <32207182+kchen445@users.noreply.github.com>
2017-10-23 22:59:33 +0900

    work on finding moves, not close to done

--- game/Piece.cpp
+++ game/Piece.cpp
@@ -10,23 +10,42 @@
     //int player;
     //bool isEmpty;
  
+vector<  vector < line > > piece::getJumpsPiece (const Board &board, point position,int rowDirection){
+	//for loop to run recursion, each recurse leads to a separate move
+	int row = position.row;
+	int col = position.col;
+	
+}
+ 
+ vector < line > piece::getJumpsKing (const Board &board, point position, bool isKing){
+	 
+}
+ 
  vector< vector<line> > piece::findMoves (const Board &board){
 	vector<line> currentJump;
 	vector< vector<line> > allJumps;
-	point start(position.row, position.col);
-	point end;
-	line move;
-	int numberOfJumps = 0;
-	bool canMove = true;
 	int row = position.row;
 	int col = position.col;
-	while(canMove){
-		numberOfJumps++;
-		int colAddition = numberOfJumps;
-		for(int rowAddition = -1; rowAddition < 2; rowAddition+=2){
-			if(board.gameBoard[row+(rowAddition*numberOfJumps)][col+colAddition].isEmpty()){
-				
+	if(!isKing){
+		while(canMove){
+			//check for empty spots to a piece's upper diagonal right and left
+			for(int rowAddition = -1; rowAddition < 2; rowAddition+=2){
+				int newRow = row + rowAddition;
+				int newCol = col + 1;
+				if(board.gameBoard[newRow][newCol].isEmpty()){
+					point end(newRow,newCol);
+					point start(row,col);
+					line move(start, end);
+					currentJump.push_back(move);
+					allJumps.push_back(currentJump);
+				} else if (board.gameBoard[newRow][newCol] != this->player){
+					vector< vector <lines> > allJumpsByPiece = getJumpsPiece(const &board, position, rowAddition);
+					allJumps.insert(allJumps.end(), allJumpsByPiece.begin(), allJumpsByPiece.end());
+				}
 			}
 		}
+	//piece is a king
+	}else{
+		
 	}
  }
--- game/Piece.h
+++ game/Piece.h
@@ -6,6 +6,22 @@
 
 #ifndef CMPT_106_CHECKERS_GAME_PIECE_H
 #define CMPT_106_CHECKERS_GAME_PIECE_H
+//  x  x
+//   []
+//     x
+//      o
+//
+//o is player one
+//x is player two
+//[] represents position after jump
+//in scenario shown above, two possible moves can be made from
+//the position after jump and therefore needs 2 vectors to represents
+//branch struct represents directions in which a piece can jump
+//given a direction
+struct branch {
+	point position;
+	std::vector <point> listOfDirectionChanges;
+}
 
 
 struct Piece {
@@ -17,9 +33,12 @@ struct Piece {
     //use this to determine whether a point on the board
     //has a proper Piece or not
     bool isEmpty;
-
+	
     Piece(point initialPosition, int player1Or2);
     Piece(point initialPosition);
+	//helper functions to find piece jumps
+	std::vector < line > getJumpsPiece (const Board &board, point position ,int rowDirection);
+	std::vector < line > getJumpsKing  (const Board &board, point position ,int rowDirection, int colDirection);
     std::vector< std::vector<line> > findMoves (const Board &board);
 };
 
