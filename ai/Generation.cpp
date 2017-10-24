09828041ae53706414d1e92e350fc9ad9e494329
barrysir <arf600@gmail.com>
2017-10-24 13:24:19 -0700

    Generation supports mutations only
    yep

--- ai/Generation.cpp
+++ ai/Generation.cpp
@@ -1,4 +1,5 @@
 #include "Generation.hpp"
+#include <sstream>
 
 //random number generation, replace it with something more C++-like if there is one
 #include <stdlib>
@@ -10,7 +11,10 @@ using namespace network;
 
 Generation::Generation(const char *directory) {
 	/*
-	//load constants from constants.ini
+	//load constants from constants.ini?
+	
+	//load innovation numbers from innovations.txt
+
 
 	//load neural networks
 	//create empty species vector
@@ -21,22 +25,56 @@ Generation::Generation(const char *directory) {
 		NNetwork* network = new NNetwork(filename);
 		species.push_back(network);
 
-	//all networks are loaded into one species, but these are separated when you call step()
+	
 	*/
 }
 
+Generation::~Generation() {
+	for (auto species : networks) {
+		for (auto network : species) {
+			delete network;
+		}
+	}
+}
+
+void Generation::save(const char *directory) {
+	//delete all files in the directory beforehand?
+
+	size_t count = 1;
+	std::ostringstream name;
+	//save each network into a sequential text file
+	for (auto species : networks) {
+		for (auto network : species) {
+			name << directory << '/' << "network" << count << ".txt";
+			network.save(name.str());
+			name.clear();
+			count++;
+		}
+	}
+}
+
 void Generation::step() {
-	separate();
-	for (auto species : networks) {
-		compete(species);
+	//mutations only: assume there is only one species
+
+	compete();	//sorts networks descending by fitness value
+	std::vector<NNetwork*>& species = networks.front();
+	
+	//repopulate generation
+	for(size_t i=GEN_KEEP; i<GEN_SIZE; i++) {	
+		//delete network
+		delete species[i];
+
+		//fill in its spot
+		NNetwork* chosen = species[rand() % GEN_KEEP];	//pick a random network
+		NNetwork* a = new NNetwork(chosen);				//put a mutated copy of it back into species
+		a.mutate(this);
+		species[i] = a;
 	}
-	for (auto species : networks) {
-		evolve(species);
-	}
-	curgenes.clear();	//generation is done; empty out all created edges?
 }
 
 size_t Generation::getInnovNum(size_t endid, size_t startid) {
+	//	if that edge has already been added that generation, returns its value in [curgenes]
+	//	otherwise, increments [curinnov] and adds the new edge to [curgenes]
 	auto it = curgenes.find({endid, startid});
 	if (it == curgenes.end()) {
 		curinnov++;
@@ -47,32 +85,68 @@ size_t Generation::getInnovNum(size_t endid, size_t startid) {
 	}
 }
 
-//complicated
-void Generation::separate();
+void compete() {
 
-//to be implemented by specific instance
-//void Generation::compete(std::vector<NNetwork*>& species);
 
-void Generation::evolve(std::vector<NNetwork*>& species) {
-	breed(species);		//do breeding
-	mutate(species);	//do mutation
-	//should these operations be inlined?
 }
+//breeding stuff
 
-//don't know how it works
-void Generation::breed(std::vector<NNetwork*>& species) {
-	//choose two networks somehow, p1 and p2
-	if (rand_double() < breed_chance) {
-		NNetwork* child = p1->breed(p2);
-		species.push_back(child);
+/*
+	//void Generation::step() {
+		//generate number of offspring based on sum of fitness values in species
+		std::vector<NNetwork*> offspring;
+		for (auto species : networks) {
+			NNetwork* child = randnetwork1.breed(randnetwork2);
+			offspring.push_back(child);
+		}
+
+		//delete all but one random organism in each species
+		//delete()
+		for (auto network : offspring) {
+			bool sorted = false;
+			for (auto species : networks) {
+				if (species.front()->difference(network) < threshold) {
+					network.push_back(species);
+					sorted = true;
+					break;
+				}
+			}
+
+			if (not(sorted)) {
+				std::vector<NNetwork*> newspecies;
+				newspecies.push_back(network);
+				network.push_back(newspecies);
+			}
+		}
+		
+
+	//complicated
+	void Generation::separate();
+
+	//to be implemented by specific instance
+	//void Generation::compete(std::vector<NNetwork*>& species);
+
+	void Generation::evolve(std::vector<NNetwork*>& species) {
+		breed(species);		//do breeding
+		mutate(species);	//do mutation
+		//should these operations be inlined?
 	}
-}
 
-void Generation::mutate(std::vector<NNetwork*>& species) {
-	for (auto network : species) {
-		if (rand_double() < mutate_chance) {
-			network->mutate(this);
-			//mutate then calls getInnovNum() when it tries to add any connections
+	//don't know how it works
+	void Generation::breed(std::vector<NNetwork*>& species) {
+		//choose two networks somehow, p1 and p2
+		if (rand_double() < breed_chance) {
+			NNetwork* child = p1->breed(p2);
+			species.push_back(child);
 		}
 	}
-}
+
+	void Generation::mutate(std::vector<NNetwork*>& species) {
+		for (auto network : species) {
+			if (rand_double() < mutate_chance) {
+				network->mutate(this);
+				//mutate then calls getInnovNum() when it tries to add any connections
+			}
+		}
+	}
+*/
--- ai/Generation.hpp
+++ ai/Generation.hpp
@@ -13,48 +13,87 @@
 namespace network {
 	class Generation {
 		private:
-			const double diff_threshold;	//the distance two networks have to be to be considered different species
-			const double breed_chance;		//the chance to breed a chosen pair of networks
-			const double mutate_chance;		//the chance to mutate a chosen network
+			//general constants
+			const size_t GEN_SIZE;	//the number of organisms in each generation
+			const size_t GEN_KEEP;	//the number of organisms to keep
+
+			//breeding constants
+			const double DIFF_THRESHOLD;	//the distance two networks have to be to be considered different species
+			const double BREED_CHANCE;		//the chance to breed a chosen pair of networks
+			const double MUTATE_CHANCE;		//the chance to mutate a chosen network
+			
 			
 			//all of the neural networks, separated into species
 			std::vector<std::vector<NNetwork*>> networks;
+
 			//lookup table of connections -> innovation numbers for all connections added this generation
 			std::unordered_map<std::pair<size_t,size_t>,size_t> curgenes;
 			size_t curinnov;	//the current highest innovation number
 
-			//Separate all networks found in [networks] into species
-			void separate();
 
 			//sorts the species by fitness value, descending
 			void compete(std::vector<NNetwork*>&) = 0;
 			
-			//evolves a species
-			//same as calling breed() then mutate()
-			void evolve(std::vector<NNetwork*>&);
 
-			//(should these be inlined in evolve()?)
-			//randomly breeds networks within a species and adds them to the species
-			void breed(std::vector<NNetwork*>&);
-			//randomly mutates networks within a species by calling NNetwork.mutate
-			void mutate(std::vector<NNetwork*>&);
+			//breeding functions
+			/*
+				//Separate all networks found in [networks] into species
+				void separate();
+
+				//evolves a species
+				//same as calling breed() then mutate()
+				void evolve(std::vector<NNetwork*>&);
+
+				//(should these be inlined in evolve()?)
+				//randomly breeds networks within a species and adds them to the species
+				void breed(std::vector<NNetwork*>&);
+				//randomly mutates networks within a species by calling NNetwork.mutate
+				void mutate(std::vector<NNetwork*>&);
+			*/
 
 		public:	
 			//Loads generation data from a directory
 			//	- constants
 			//	- all neural networks (which are put into a single species)
 			Generation(const char *directory);
+			void Generation::save(const char *directory);
 
-			//Evolves everything forward a generation
-			//	Calls separate(), then compete() on each species, then evolve() on each species
-			//	clears out curgenes after finished
+			~Generation();
+
+			//Evolves everything forward a generation.
 			void step();
 
 			//given an edge that a network wants to add, returns the innovation number that edge should take
-			//	if that edge has already been added that generation, returns its value in [curgenes]
-			//	otherwise, increments [curinnov] and adds the new edge to [curgenes]
 			//to be called by NNetwork when mutating
 			size_t getInnovNum(size_t endid, size_t startid);
 	};
 }
+
+/*double NNetwork::difference (NNetwork& other) {
+	size_t E = 0;
+	size_t D = 0;
+	size_t N = 0;
+
+
+	double W = 0;
+	size_t Wcount = 0;
+
+	auto it = this.conns.begin();
+	auto jt = other.conns.begin();
+
+	if (it->innov == jt->innov) {
+		W += abs(it->weight - jt->weight);
+		Wcount++;
+		it++;
+		jt++;
+	} else {
+		D++;
+		if (it->innov < jt->innov) {
+			it++;
+		} else {
+			jt++;
+		}
+	}
+
+}*/
 #endif
