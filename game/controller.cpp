a573b07721b8b364d00de5bad02acc502e0926c1
Miraion <j.schwartz564@icloud.com>
2017-11-20 13:15:24 -0800

    Game Controller Integration
     - Added pre processor commands to disable the display in the CheckersController class.
     - CheckersController now extends cl::game_template<AIPlayer>
     - ml/main.cpp is now set up to be the main trining source file for the training executable.

--- game/controller.cpp
+++ game/controller.cpp
@@ -5,27 +5,34 @@
 #include "AIPlayer.h"
 #include "Display.h"
 
+#include "../ml/syscl.hpp" // for machine learning system
+
+// #define DISABLE_DISPLAY before including this file for training system
 
 using namespace std;
 
-struct Data{
-    int turns;
-    bool win;
-    bool loss;
-    bool tie;
-    int piecesLost;
-    Data(int turns, bool win, bool loss, bool tie, int piecesLost)
-    :turns(turns),win(win),loss(loss),tie(tie),piecesLost(piecesLost)
-    {}
-};
+//struct Data{
+//    int turns;
+//    bool win;
+//    bool loss;
+//    bool tie;
+//    int piecesLost;
+//    Data(int turns, bool win, bool loss, bool tie, int piecesLost)
+//    :turns(turns),win(win),loss(loss),tie(tie),piecesLost(piecesLost)
+//    {}
+//};
+
+using Data = cl::player_data;
 
 
-struct CheckerController{
+struct CheckerController : public cl::game_template<AIPlayer> {
 	//order of player parameters matter
 	//board will treat the first player passed in as player 1
 	//and have their pieces at the bottom
     array<Data,2> gameLoop(Player* player1, Player* player2){
+#ifndef DISABLE_DISPLAY
         Display display;
+#endif
         Board board;
 
         bool tie = false;
@@ -48,25 +55,39 @@ struct CheckerController{
         //actual game loop
         while (listOfPlayers[0]->canMove || listOfPlayers[1]->canMove) {
 
+#ifndef DISABLE_DISPLAY
             display.displayGame(board.gameBoard);
+#endif
+            
+#ifndef DISABLE_DISPLAY
             cout << "Player 1's move" << endl;
+#endif
             listOfPlayers[0]->findMoves(board.gameBoard);
 
             //canMove is updated inside getMove
             listOfPlayers[0]->getMove(board.gameBoard);
             if(!listOfPlayers[0]->canMove){break;}
             board.update(listOfPlayers,0);
+            
+#ifndef DISABLE_DISPLAY
             cout << "Player 1 move: " << listOfPlayers[0]->movesAsString.at(listOfPlayers[0]->indexOfMove) << endl;
+#endif
 
-
+#ifndef DISABLE_DISPLAY
             display.displayGame(board.gameBoard);
+#endif
+            
+#ifndef DISABLE_DISPLAY
             cout << "Player 2's move" << endl;
+#endif
             listOfPlayers[1]->findMoves(board.gameBoard);
             listOfPlayers[1]->getMove(board.gameBoard);
             if(!listOfPlayers[1]->canMove){break;}
             board.update(listOfPlayers,1);
+            
+#ifndef DISABLE_DISPLAY
             cout << "Player 2 move: " << listOfPlayers[1]->movesAsString.at(listOfPlayers[1]->indexOfMove) << endl;
-
+#endif
 
             turns++;
             //https://boardgames.stackexchange.com/questions/34659/how-many-turns-does-an-average-game-of-checkers-draughts-go-for
@@ -79,6 +100,8 @@ struct CheckerController{
 
         player1LostPieces = listOfPlayers[0]->findLostPieces();
         player2LostPieces = listOfPlayers[1]->findLostPieces();
+        
+#ifndef DISABLE_DISPLAY
         if(!listOfPlayers[1]->canMove){
             cout << endl << "Player 1 wins!" << endl << endl;
         }else if (!listOfPlayers[0]->canMove){
@@ -99,10 +122,18 @@ struct CheckerController{
                 cout << "Stalemate with Player 2 taking more pieces!" << endl;
             }
         }
+#endif
 
         Data dataPlayer1(turns,listOfPlayers[0]->canMove,player1Loss,tie,player1LostPieces);
         Data dataPlayer2(turns,listOfPlayers[1]->canMove,player2Loss,tie,player2LostPieces);
         array<Data,2> arr = {dataPlayer1,dataPlayer2};
         return arr;
     }
-};
+    
+    
+    // Override from cl::game_template<AIPlayer>c
+    array<Data, 2> compete (AIPlayer const &e1, AIPlayer const &e2) override {
+        return gameLoop(&e1, &e2);
+    }
+    
+}; // struct CheckersController
--- ml/main.cpp
+++ ml/main.cpp
@@ -1,100 +1,26 @@
+// This is the main training source file.
+
 #include <iostream>
 #include <ctime>
 
-#define NEURAL_IN   3
-#define NEURAL_OUT  3
-#define NUM_THREADS 4
+#define NEURAL_IN   64
+#define NEURAL_OUT  32
 
-#include "sysml.hpp"
-#include "network.hpp"
-#include "systl.hpp"
-#include "../ai/NNetwork.hpp"
+#include "tournament.hpp"
 
-template<typename T>
-std::string as_str (std::vector<T> const &v) {
-    std::string str = "[";
-    for (size_t i = 0; i < v.size(); ++i) {
-        str += std::to_string(v[i]);
-        if (i != v.size() - 1) {
-            str += ", ";
-        }
-    }
-    str += "]";
-    return str;
-}
-
-template<typename T, size_t N>
-std::string as_str (std::array<T, N> const &v) {
-    std::string str = "[";
-    for (size_t i = 0; i < N; ++i) {
-        str += std::to_string(v[i]);
-        if (i != v.size() - 1) {
-            str += ", ";
-        }
-    }
-    str += "]";
-    return str;
-}
-
-
-template<typename T, size_t N>
-std::string as_str (std::vector<std::array<T, N>> const &v) {
-    std::string str = "{";
-    for (size_t i = 0; i < v.size(); ++i) {
-        str += as_str(v[i]);
-        if (i != v.size() - 1) {
-            str += ", ";
-        }
-    }
-    str += "}";
-    return str;
-}
-
+#define DISABLE_DISPLAY
+#include "../game/controller.cpp"
 
 int main() {
     srand((unsigned int)time(nullptr));
 
+    ptr<cl::game_template<AIPlayer>> game{new CheckerController{}};
 
-    ml::cfg::global->pause_on_convergence = false;
-    ml::cfg::global->convergence_interval = 1000;
-    ml::cfg::global->display_update_time = 500; // update time in milliseconds
-    ml::cfg::global->use_goal = true;
-    ml::cfg::global->goal = 0.01;
+    // Load network from save path, or define a brand new one.
+    ptr<ml::network_o> seed_net{new network::NNetwork<NEURAL_IN, NEURAL_OUT>{ml::cfg::global->save_path}};
 
+    cl::tournament_set training_set{seed_net, game};
 
-//    auto n = ptr<ml::network_o>{new network::NNetwork<NEURAL_IN, NEURAL_OUT>{
-//            "/Users/Jeremy/Desktop/C++/cmpt-106-checkers/scripts/net.txt"
-//    }};
-
-    auto n = ptr<ml::network_o>{new network::NNetwork<NEURAL_IN, NEURAL_OUT>{std::vector<size_t>{7}}};
-
-    std::vector<std::array<double, NEURAL_IN>> inputs {
-            {0,0,0}, 
-            {0,0,1}, 
-            {0,1,0}, 
-            {0,1,1},
-            {1,0,0},
-            {1,0,1},
-            {1,1,0},
-            {1,1,1}
-    };
-
-    std::vector<std::array<double, NEURAL_OUT>> outputs {
-            {1,1,1}, 
-            {1,1,0}, 
-            {1,0,1}, 
-            {1,0,0},
-            {0,1,1},
-            {0,1,0},
-            {0,0,1},
-            {0,0,0}
-    };
-
-    ptr<ml::io_set> tp{new ml::io_set{inputs, outputs}};
-    std::cout << "Starting..." << std::endl;
-    tl::trainer trainer{n, tp};
-    auto best = trainer.train();
-
-    ml::io_set sol{best->network, inputs};
-    std::cout << as_str(sol.outputs()) << std::endl;
+    training_set.run_training(1000); // run 1000 generations
+    training_set.save(); // save the final result
 }
