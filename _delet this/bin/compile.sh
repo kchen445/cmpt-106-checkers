903e3e1b87479c6e215717b0edcb00d171dfa950
Miraion <j.schwartz564@icloud.com>
2017-11-29 09:36:35 -0800

    FINAL!!!!!
    hopefully

--- _delet this/bin/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../ml
-make
-make exe
-cd ../bin
--- _delet this/scripts/createGeneration.py
+++
@@ -1,8 +0,0 @@
-import os
-
-numNetworks = 100
-
-for i in range(0, numNetworks):
-    fname = 'G{}N{}.txt'.format(0, i)
-    cmd = "python createNetwork.py > ../test/networks/test/" + fname
-    os.system(cmd)
